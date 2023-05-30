82c4127b05f7df888cee81f219597865e7e0404c
gabriel.rosch <gabriel.rosch@veolia.com>
2023-05-29 22:42:15 -0300

    first commit

--- src/components/Veiculo.jsx
+++ src/components/Veiculo.jsx
@@ -3,32 +3,44 @@
 //C - CREATE - [OK]
 //
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button, Form, Table } from 'react-bootstrap';
 import styles from './Veiculo.module.css'
+import api from "../service/api";
 
 function Veiculo() {
   let [veiculos, setVeiculos] = useState([]);
   let [marca, setMarca] = useState('');
   let [modelo, setModelo] = useState('');
   let [ano, setAno] = useState('');
-  let [cor, setCor] = useState('');
+  let [placa, setPlaca] = useState('');
 
-  function salvar() {
+  async function salvar() {
     let veiculo = {
       marca: marca,
       modelo: modelo,
-      ano: ano,
-      cor: cor
+      ano: Number(ano),
+      placa: placa
     }
-    veiculos.push(veiculo)
+    
+    const resposta = await api.post('/veiculos',veiculo)
+    .catch((e)=>{
+      alert(e.response.data.msg)
+    })
+    
+    veiculos.push(resposta.data)
+
     setVeiculos([...veiculos])
+
     limparForm()
   }
 
-  function excluir(veiculo) {
+  async function excluir(veiculo) {
+
+      const resposta = await api.delete(`/veiculos/${veiculo.placa}`)
+
       veiculos.forEach((v,i)=>{
-        if(veiculo.marca == v.marca) {
+        if(resposta.data.placa == v.placa) {
           veiculos.splice(i,1)
         }
       }) 
@@ -37,14 +49,31 @@ function Veiculo() {
   //limpar forme
   function limparForm() {
     setAno('')
-    setCor('')
+    setPlaca('')
     setMarca('')
     setModelo('')
+  }
+
+  useEffect(()=>{
+    buscarVeiculos();
+  },[])
+
+  async function buscarVeiculos() {
+    //array veiculos
+    const resposta = await api.get('/veiculos') 
+    setVeiculos(resposta.data)
   }
 
   return (
     <div className={styles.main}>
       <h1>Veiculo</h1>
+      <Form.Control
+        value={placa}
+        onChange={(e)=>{setPlaca(e.target.value)}}
+        placeholder="Placa"
+        aria-label="Placa"
+        aria-describedby="basic-addon1"
+      />
       <Form.Control
         value={marca}
         onChange={(e)=>{setMarca(e.target.value)}}
@@ -66,21 +95,14 @@ function Veiculo() {
         aria-label="Ano"
         aria-describedby="basic-addon1"
       />
-      <Form.Control
-        value={cor}
-        onChange={(e)=>{setCor(e.target.value)}}
-        placeholder="Cor"
-        aria-label="Cor"
-        aria-describedby="basic-addon1"
-      />
       <Button onClick={salvar}>Salvar</Button>
       <Table striped bordered hover variant="dark">
         <thead>
           <tr>
+            <td>Placa</td>
             <td>Marca</td>
             <td>Modelo</td>
             <td>Ano</td>
-            <td>Cor</td>
             <td></td>
           </tr>
         </thead>
@@ -88,12 +110,11 @@ function Veiculo() {
         { 
           veiculos.map((veiculo, i) => {
             return (
-
               <tr key={i}>
+                <td>{veiculo.placa}</td>
                 <td>{veiculo.marca}</td>
                 <td>{veiculo.modelo}</td>
                 <td>{veiculo.ano}</td>
-                <td>{veiculo.cor}</td>
                 <td>
                   <Button onClick={()=>{excluir(veiculo)}}>x</Button>
                 </td>
